0673c534051833f031b6cc0bb64caface9986817
Chevy403 <chevychuty@gmail.com>
2025-02-04 21:58:28 -0600

    puse el archivo QUEY

---
+++ Critopher/QUEY.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE ejemplito;  -- Asi se crea la BD
+
+USE ejemplito; -- Asi se le indica que se va a usar esa BD
+
+CREATE TABLE usuario(
+usuario_id INT AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(100) NOT NULL, 
+email VARCHAR (100) NOT NULL UNIQUE,
+estado ENUM ( 'activo', 'inactivo') DEFAULT 'activo',
+fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
+
+ALTER TABLE usuario
+ADD COLUMN telefono VARCHAR(20)AFTER email;
+
+ALTER TABLE usuario
+MODIFY COLUMN nombre VARCHAR(200) NOT NULL;
+
+ALTER TABLE usuario
+DROP COLUMN telefono;
+
+DROP TABLE IF EXISTS usuario;
+
+DROP DATABASE IF EXISTS ejemplito;
+
+
+
+
+
+
+
+
+
+
